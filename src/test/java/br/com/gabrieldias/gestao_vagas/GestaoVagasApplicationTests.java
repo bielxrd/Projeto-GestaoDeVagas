f8b03b0fa311cf169a2760fe6ef46be07ca0074b
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-26 20:26:05 -0300

    Estrutura criada pelo Initilalzr:

---
+++ src/test/java/br/com/gabrieldias/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
