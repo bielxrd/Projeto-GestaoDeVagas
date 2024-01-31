6eeecb8317bfcd78f0cd52a446c7f39737232497
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-31 16:22:47 -0300

    DTO para erros e utilizar para transicao de dados

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+
+    private String field;
+
+}
