874091edfbe57200910bd0d701098b235b915caa
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-01-31 16:23:49 -0300

    ExceptionHandlerController, Controller para tratar excecao methodargumentnotvalidexception e anotacoes @ControllerAdvice e @ExceptionHandler)

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,35 @@
+package br.com.gabrieldias.gestao_vagas.exceptions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorMessageDTO>> handleMehodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<ErrorMessageDTO> dto = new ArrayList<>();
+        e.getBindingResult().getFieldErrors().forEach(err -> {
+            String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
+            ErrorMessageDTO errorMessageDTO = new ErrorMessageDTO(message, err.getField());
+            dto.add(errorMessageDTO);
+        });
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
