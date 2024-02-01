db256f25f3512639e9005278692306d3f359a35f
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-01 15:23:06 -0300

    Metodo FindByUsernameOrEmail

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -5,7 +5,9 @@ import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
-
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
