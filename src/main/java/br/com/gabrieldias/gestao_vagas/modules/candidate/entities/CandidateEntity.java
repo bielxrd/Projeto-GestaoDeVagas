f41cb61b88106109178c215f3d2d843a2fbb78be
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-01 15:08:12 -0300

    Anotacao @Entity para criacao da tabela no bd, @Id e @GeneratedValue para PK e Gerenciar valores com base em UUID

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -1,20 +1,30 @@
 package br.com.gabrieldias.gestao_vagas.modules.candidate.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "tb_candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @Pattern(regexp = "^[^\\s]+$", message = "O campo [username] não deve conter espaços.")
@@ -28,5 +38,8 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
