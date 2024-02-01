fc9413d1e5d473d30a4e2296b7aa841b08049c0e
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-01 15:07:24 -0300

    Utilizando JPA Repository e salvando no bd

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,17 +2,21 @@ package br.com.gabrieldias.gestao_vagas.modules.candidate.controllers;
 
 
 import br.com.gabrieldias.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato : " + candidateEntity.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
     }
 
 
