7b9f3bce7d3f05ee42144e26bf6c3aa36407abcf
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-02 17:15:21 -0300

    Caso de uso para tratar validacao do usuario

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,25 @@
+package br.com.gabrieldias.gestao_vagas.modules.candidate.useCases;
+
+import br.com.gabrieldias.gestao_vagas.exceptions.UserFoundException;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.candidateRepository.save(candidateEntity);
+    }
+
+}
