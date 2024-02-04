81229acc845d464d184d61d472400d336c7b36dd
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-04 15:11:24 -0300

    CompanyRepository para manipulacao de dados ORM

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,14 @@
+package br.com.gabrieldias.gestao_vagas.modules.company.repositories;
+
+import br.com.gabrieldias.gestao_vagas.modules.company.entities.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+    Optional<CompanyEntity> findByUsernameOrEmailOrName(String username, String email, String name);
+
+}
