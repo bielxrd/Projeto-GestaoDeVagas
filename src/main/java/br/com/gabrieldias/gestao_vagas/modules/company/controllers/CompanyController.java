e7f3d20e6517e06a5b90453527748937050e3da8
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-02-28 17:12:17 -0300

    feat: metodo post para cadastro de companyentity

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,29 @@
+package br.com.gabrieldias.gestao_vagas.modules.company.controllers;
+
+import br.com.gabrieldias.gestao_vagas.exceptions.UserFoundException;
+import br.com.gabrieldias.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.gabrieldias.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+        try {
+            CompanyEntity company = createCompanyUseCase.execute(companyEntity);
+            return ResponseEntity.ok().body(company);
+        } catch (UserFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
