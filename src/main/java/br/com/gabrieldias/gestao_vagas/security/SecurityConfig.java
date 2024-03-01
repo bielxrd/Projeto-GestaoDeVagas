c9f731829464735dfa1fe37b8403a41c811a63fb
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-03-01 01:13:55 -0300

    feat: adiciona camada de seguranca, desativa csrf para configurar manualmente

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.gabrieldias.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/candidate/").permitAll();
+            auth.requestMatchers("/company/").permitAll();
+            auth.anyRequest().authenticated();
+        });
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
