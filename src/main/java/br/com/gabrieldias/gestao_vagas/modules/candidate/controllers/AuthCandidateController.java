09624d5be3e47b725b8e09fe7d946a577bf5fc00
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-03-05 09:36:41 -0300

    feat: rota para autenticacao do usuario

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -0,0 +1,28 @@
+package br.com.gabrieldias.gestao_vagas.modules.candidate.controllers;
+
+import br.com.gabrieldias.gestao_vagas.modules.candidate.dto.AuthCandidateDTO;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.useCases.AuthCandidateUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCandidateController {
+
+    @Autowired
+    private AuthCandidateUseCase authCandidateUseCase;
+
+
+    @PostMapping("/candidate")
+    public ResponseEntity login(@RequestBody AuthCandidateDTO authCandidateDTO) {
+        try {
+            String token = authCandidateUseCase.authenticate(authCandidateDTO);
+            return ResponseEntity.ok().body(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+
+}
