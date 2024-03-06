7a4e941138ba1eea7aa022d2572876d0b187cd46
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-03-06 11:12:42 -0300

    featÃ: DTO para JobEntity

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,12 @@
+package br.com.gabrieldias.gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+    private String description;
+    private String benefits;
+    private String level;
+
+}
