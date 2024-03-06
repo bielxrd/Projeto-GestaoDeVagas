f684f435f3761af9e9408d21ff4362fb8583294e
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-03-06 11:10:31 -0300

    featÃ: adiciona filter de autenticacao para rotas que necessitam autenticacao

--- src/main/java/br/com/gabrieldias/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/security/SecurityConfig.java
@@ -1,25 +1,32 @@
 package br.com.gabrieldias.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecuriyFilter securiyFilter;
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable());
         httpSecurity.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/candidate/").permitAll();
-            auth.requestMatchers("/company/").permitAll();
-            auth.requestMatchers("/auth/company").permitAll();
-            auth.requestMatchers("/auth/candidate").permitAll();
-            auth.anyRequest().authenticated();
-        });
+                    auth.requestMatchers("/candidate/").permitAll();
+                    auth.requestMatchers("/company/").permitAll();
+                    auth.requestMatchers("/auth/company").permitAll();
+                    auth.requestMatchers("/auth/candidate").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(securiyFilter, BasicAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/security/SecuriyFilter.java
@@ -0,0 +1,45 @@
+package br.com.gabrieldias.gestao_vagas.security;
+
+import br.com.gabrieldias.gestao_vagas.providers.JWTProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class SecuriyFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization != null) {
+            String subjectToken = jwtProvider.validateToken(authorization);
+
+            if (subjectToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            request.setAttribute("company_id", subjectToken);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
