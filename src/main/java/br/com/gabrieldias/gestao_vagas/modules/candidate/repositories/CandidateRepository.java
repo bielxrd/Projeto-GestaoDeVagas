1adeadcf5e9c11a305b78845a026a52afecafd7b
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-03 03:53:42 -0300

    fix

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -1,13 +1,13 @@
 package br.com.gabrieldias.gestao_vagas.modules.candidate.repositories;
 
 import br.com.gabrieldias.gestao_vagas.modules.candidate.entities.CandidateEntity;
-import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-@Repository
+
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 
