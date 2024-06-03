ddbdb340a4fa82dd830ffc13503366df600fd115
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-03 05:42:20 -0300

    feat: provider do jwt do candidato

---
+++ src/main/java/br/com/gabrieldias/gestao_vagas/providers/JWTCandidateProvider.java
@@ -0,0 +1,32 @@
+package br.com.gabrieldias.gestao_vagas.providers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTCandidateProvider {
+
+    @Value("${user.security.token.secret}")
+    public String secretCandidateKey;
+
+    public DecodedJWT validateCandidateToken(String token) {
+        token = token.replace("Bearer ", "");
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretCandidateKey);
+
+            DecodedJWT tokenValidated = JWT.require(algorithm).build().verify(token);
+
+            return tokenValidated;
+        } catch (JWTVerificationException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
