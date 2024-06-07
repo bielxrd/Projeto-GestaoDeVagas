6e00fe72cb32d50e389a39600231502fd3df45eb
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-07 03:30:47 -0300

    fix: retorna o token descodificado

--- src/main/java/br/com/gabrieldias/gestao_vagas/providers/JWTProvider.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/providers/JWTProvider.java
@@ -3,6 +3,7 @@ package br.com.gabrieldias.gestao_vagas.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +13,15 @@ public class JWTProvider {
     @Value("${security.token.secret}")
     private String secretKey;
 
-    public String validateToken(String token) {
+    public DecodedJWT validateToken(String token) {
         token = token.replace("Bearer ", "");
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
-            String subject = JWT.require(algorithm).build().verify(token).getSubject();
+            DecodedJWT subject = JWT.require(algorithm).build().verify(token);
             return subject;
         } catch (JWTVerificationException e) {
             e.printStackTrace();
-            return "";
+            return null;
         }
     }
 
