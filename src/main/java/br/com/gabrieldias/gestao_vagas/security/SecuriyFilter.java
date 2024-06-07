fe79f4331a96af97f80dc225be0488b706ad05b7
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-07 03:31:08 -0300

    feat: manda roles para o contexto da seguranca

--- src/main/java/br/com/gabrieldias/gestao_vagas/security/SecuriyFilter.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/security/SecuriyFilter.java
@@ -1,18 +1,21 @@
 package br.com.gabrieldias.gestao_vagas.security;
 
 import br.com.gabrieldias.gestao_vagas.providers.JWTProvider;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 public class SecuriyFilter extends OncePerRequestFilter {
@@ -27,17 +30,22 @@ public class SecuriyFilter extends OncePerRequestFilter {
 
         if(request.getRequestURI().startsWith("/company")) {
             if (authorization != null) {
-                String subjectToken = jwtProvider.validateToken(authorization);
+               DecodedJWT subjectToken = jwtProvider.validateToken(authorization);
 
-                if (subjectToken.isEmpty()) {
+                if (subjectToken == null) {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                     return;
                 }
 
-                request.setAttribute("company_id", subjectToken);
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+                List<Object> roles = subjectToken.getClaim("roles").asList(Object.class);
+
+                List<SimpleGrantedAuthority> listRolesPrefixed = roles.stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())).toList();
+
+                request.setAttribute("company_id", subjectToken.getSubject());
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken.getSubject(), null, listRolesPrefixed);
                 SecurityContextHolder.getContext().setAuthentication(auth);
-                System.out.println(subjectToken);
+
             }
         }
 
