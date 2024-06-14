845af57586b54412a22fd088636ec74af735a4ac
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-14 02:14:02 -0300

    schema para token dto

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.gabrieldias.gestao_vagas.modules.candidate.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthCandidateResponseDTO {
 
+    @Schema(example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     private String acess_token;
 
     private Long expiresIn;
--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/dto/AuthCompanyResponseDTO.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/dto/AuthCompanyResponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.gabrieldias.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthCompanyResponseDTO {
-
+    @Schema(example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     public String acess_token;
     public Long expiresIn;
 }
