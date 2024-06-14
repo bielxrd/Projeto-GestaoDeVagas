481b603541c67ba40ae9ba9c38e16a8452744e43
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-14 02:13:09 -0300

    response para login da company

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -6,6 +6,10 @@ import br.com.gabrieldias.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.gabrieldias.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import br.com.gabrieldias.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,6 +31,11 @@ public class AuthCompanyController {
 
     @PostMapping("/auth")
     @Operation(summary = "Responsável por efetuar o login da company e retornar um JWT Token.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = AuthCompanyResponseDTO.class))
+            })
+    })
     public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
         try {
            AuthCompanyResponseDTO token = this.authCompanyUseCase.execute(authCompanyDTO);
