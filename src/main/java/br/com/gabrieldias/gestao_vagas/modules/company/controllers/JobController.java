e7b990928ed49095c84801c6f97f7bda9c9ed068
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-14 01:33:44 -0300

    body da resposta do metodo create

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/JobController.java
@@ -4,6 +4,10 @@ import br.com.gabrieldias.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.gabrieldias.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.gabrieldias.gestao_vagas.modules.company.useCases.CreateJobUseCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,6 +33,11 @@ public class JobController {
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
     @Operation(summary = "Responsável por realizar um cadastro de uma vaga, necessário estar autenticado.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class))
+            })
+    })
     @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest httpServletRequest) {
 
