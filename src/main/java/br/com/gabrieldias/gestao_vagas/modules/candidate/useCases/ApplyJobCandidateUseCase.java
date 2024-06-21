f131c686ed2f0a8ad7f6ac1b2760c1b23138a35c
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-21 20:33:08 -0300

    feat: funcionalidade para aplicar a uma vaga

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -2,10 +2,11 @@ package br.com.gabrieldias.gestao_vagas.modules.candidate.useCases;
 
 import br.com.gabrieldias.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.gabrieldias.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.entities.ApplyJobEntity;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.repositories.ApplyJobRepostory;
 import br.com.gabrieldias.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import br.com.gabrieldias.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -19,15 +20,22 @@ public class ApplyJobCandidateUseCase {
     @Autowired
     private JobRepository jobRepository;
 
-    public void applyJob(UUID idCandidate, UUID idJob) { // Necessita do ID da vaga e do CANDIDATO
-       // Validar se o candidato existe.
+    @Autowired
+    private ApplyJobRepostory applyJobRepostory;
+
+    public ApplyJobEntity applyJob(UUID idCandidate, UUID idJob) { // Necessita do ID da vaga e do CANDIDATO
+        // Validar se o candidato existe.
         this.candidateRepository.findById(idCandidate).orElseThrow(() -> new UserNotFoundException("User Not Found"));
 
         // Validar se a vaga existe.
         this.jobRepository.findById(idJob).orElseThrow(() -> new JobNotFoundException("Job not found"));
 
         // Candidato se candidata na vaga
+        ApplyJobEntity applyJob = ApplyJobEntity.builder()
+                .idCandidate(idCandidate)
+                .idJob(idJob).build();
 
+       return applyJobRepostory.save(applyJob); // Salva no banco de dados
 
     }
 
