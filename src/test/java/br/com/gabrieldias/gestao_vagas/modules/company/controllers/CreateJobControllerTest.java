9f651f9724c881bf7e87dd05f9f114a84ad83ac7
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-22 01:03:59 -0300

    feat: teste para cadastro de job

--- src/test/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,29 +1,30 @@
 package br.com.gabrieldias.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import br.com.gabrieldias.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.gabrieldias.gestao_vagas.utils.TestUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.UUID;
-
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CreateJobControllerTest {
 
     private MockMvc mockMvc;
@@ -33,7 +34,9 @@ public class CreateJobControllerTest {
 
     @Before
     public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
@@ -50,7 +53,8 @@ public class CreateJobControllerTest {
         ResultActions resultActions = mockMvc.perform(post("/company/job/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(TestUtils.objectToJson(createJob))
-                        .header("Authorization", TestUtils.generateTokenTest(UUID.randomUUID())))
+                        .header("Authorization",
+                                TestUtils.generateTokenTest(UUID.fromString("1e19e853-98ac-4bc3-9d8d-6b2d906077ce"))))
                 .andExpect(status().isOk());
 
         System.out.println(resultActions);
