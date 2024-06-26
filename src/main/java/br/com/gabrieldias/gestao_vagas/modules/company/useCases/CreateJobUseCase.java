367a3d9719ac98afa1e2c2d9428219a97855ac9e
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-26 16:42:37 -0300

    feat: valida se company nao existe

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,7 +1,9 @@
 package br.com.gabrieldias.gestao_vagas.modules.company.useCases;
 
 
+import br.com.gabrieldias.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.gabrieldias.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.gabrieldias.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.gabrieldias.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,13 @@ public class CreateJobUseCase {
     @Autowired
     private JobRepository jobRepository;
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
     public JobEntity execute(JobEntity jobEntity) {
+
+        companyRepository.findById(jobEntity.getCompanyId()).orElseThrow(() -> new CompanyNotFoundException("Company not found"));
+
         return this.jobRepository.save(jobEntity);
     }
 
