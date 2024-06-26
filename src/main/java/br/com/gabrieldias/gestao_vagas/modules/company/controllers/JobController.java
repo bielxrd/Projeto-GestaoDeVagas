8b2f11e5e82fb9b1539892695468ebbae7431045
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-26 16:42:19 -0300

    featÃ: exception para company not found~

--- src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,5 +1,6 @@
 package br.com.gabrieldias.gestao_vagas.modules.company.controllers;
 
+import br.com.gabrieldias.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.gabrieldias.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.gabrieldias.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.gabrieldias.gestao_vagas.modules.company.useCases.CreateJobUseCase;
@@ -12,6 +13,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -43,7 +45,11 @@ public class JobController {
 
         Object companyId = httpServletRequest.getAttribute("company_id");
 
-        JobEntity jobEntity = JobEntity.builder().benefits(createJobDTO.getBenefits()).companyId(UUID.fromString(companyId.toString())).description(createJobDTO.getDescription()).level(createJobDTO.getLevel()).build();
+        if (companyId == null) {
+            throw new CompanyNotFoundException("Company not found.");
+        }
+
+        JobEntity jobEntity = JobEntity.builder().benefits(createJobDTO.getBenefits()).companyId(UUID.fromString((String) companyId)).description(createJobDTO.getDescription()).level(createJobDTO.getLevel()).build();
 
         JobEntity job = createJobUseCase.execute(jobEntity);
 
