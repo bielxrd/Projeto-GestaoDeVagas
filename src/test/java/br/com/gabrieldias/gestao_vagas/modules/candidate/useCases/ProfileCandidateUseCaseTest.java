6c524ac4200d3f7b0094d7c52724ce4cb748b5f3
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-26 02:06:31 -0300

    feat: testa pegar o profile de um candidato com uuid errado

---
+++ src/test/java/br/com/gabrieldias/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCaseTest.java
@@ -0,0 +1,46 @@
+package br.com.gabrieldias.gestao_vagas.modules.candidate.useCases;
+
+import br.com.gabrieldias.gestao_vagas.exceptions.CandidateNotFoundException;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import br.com.gabrieldias.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProfileCandidateUseCaseTest {
+
+    @InjectMocks
+    private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Test
+    @DisplayName("Should not be able to get an profile candidate that not exists on our repository")
+    public void should_not_be_able_to_get_an_profile_candidate_that_not_exists() {
+
+        CandidateEntity candidate = new CandidateEntity();
+        candidate.setId(UUID.randomUUID());
+
+        when(this.candidateRepository.findById(candidate.getId())).thenReturn(Optional.empty());
+
+        try {
+            this.profileCandidateUseCase.getCandidateProfile(candidate.getId());
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(CandidateNotFoundException.class);
+        }
+
+    }
+
+
+}
