484d380ae7d2011eac2588502e57d3b985120ea8
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-26 19:06:16 -0300

    rename

--- src/main/java/br/com/gabrieldias/gestao_vagas/security/SecurityCompanyFilter.java
+++ src/main/java/br/com/gabrieldias/gestao_vagas/security/SecurityCompanyFilter.java
@@ -14,11 +14,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 @Component
-public class SecuriyFilter extends OncePerRequestFilter {
+public class SecuriyCompanyFilter extends OncePerRequestFilter {
 
     @Autowired
     private JWTProvider jwtProvider;
